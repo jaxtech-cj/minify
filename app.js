f5bfaedf85d54cdfeab17a8f347d92e6078518d6
Corey Jackson <cjaxsn@gmail.com>
2026-06-06 11:29:59 -0400

    v1 images

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+/**
+ * @file Minifier
+ * @since 1.0.0
+ * @module app.js
+ * @author Corey Jackson <cjaxsn@gmail.com>
+ * @requires imagemin, imagemin-webp
+ * @copyright Jax Tech
+ */
+
+import imagemin from 'imagemin';
+import imageminWebp from 'imagemin-webp';
+
+console.log("Minify Utility by Jax Tech");
+//process.exit()
+
+const files = await imagemin(['images/*.{jpg,webp}'], {
+	destination: 'images/min',
+	plugins: [
+		imageminWebp({quality: 75})
+	]
+});
+
+console.log(files);
+console.log('Images optimized');
