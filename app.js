4967207e668aad1ab75667e887c4f17a53cfb6ae
Corey Jackson <cjaxsn@gmail.com>
2026-06-06 20:04:44 -0400

    lightning css

--- app.js
+++ app.js
@@ -9,9 +9,27 @@
 
 import imagemin from 'imagemin';
 import imageminWebp from 'imagemin-webp';
+import { bundle } from 'lightningcss';
+import fs from 'fs';
+import { glob } from 'glob';
 
 console.log("Minify Utility by Jax Tech");
-//process.exit()
+
+function minifyCSS() {
+	// Find all CSS files in your source directory
+	//const cssfiles = glob.sync('src/**/*.css');
+
+	// Process and minify the external file
+	let { code, map } = bundle({
+		filename: 'css/all.css',
+		minify: true,
+		sourceMap: true // Optional: Generates a source map
+	});
+
+	// Write the minified payload to disk
+	fs.writeFileSync('css/min/all.min.css', code);
+	console.log("CSS Minifying Complete");
+}
 
 const files = await imagemin(['images/*.{jpg,webp}'], {
 	destination: 'images/min',
