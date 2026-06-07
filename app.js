60dbfd37ae89aa85faf5866d1e8c88845d18537b
Corey Jackson <cjaxsn@gmail.com>
2026-06-06 21:35:29 -0400

    minify js uglify-js

--- app.js
+++ app.js
@@ -9,16 +9,52 @@
 
 import imagemin from 'imagemin';
 import imageminWebp from 'imagemin-webp';
+import fs  from 'fs';
 import { bundle } from 'lightningcss';
-import { fs } from 'fs';
 import { glob } from 'glob';
+import UglifyJS from 'uglify-js';
+
 
 console.log("Minify Utility by Jax Tech");
 
-minifyCSS();
-minifyImages();
+minifyJS();
+//minifyCSS();
+//minifyImages();
 
 console.log("Minify Complete");
+
+function minifyJS()
+{
+	console.log("Minifying JS");
+	const inputFile = 'js/main.js';
+	const outputFile = 'js/main.min.js';
+
+	const sourceCode = fs.readFileSync(inputFile, 'utf8');
+	console.log(sourceCode);
+	
+	// Configuration options for UglifyJS
+    const options = {
+        mangle: true, // Shorten variable and function names
+        compress: {
+            dead_code: true, // Remove unreachable code
+            drop_debugger: true, // Remove debugger statements
+            drop_console: false // Set to true to strip console.log statements
+        }
+    };
+	// Perform minification
+    const result = UglifyJS.minify(sourceCode, options);
+
+    // Check for compilation errors
+    if (result.error) {
+        throw result.error;
+    }
+
+    // Save the minified code to the output file
+    fs.writeFileSync(outputFile, result.code, 'utf8');
+    console.log(`Success! Minified file saved to: ${outputFile}`);
+
+	  console.log("JS Minifying Complete");
+}
 
 function minifyCSS() {
 	// Find all CSS files in your source directory
