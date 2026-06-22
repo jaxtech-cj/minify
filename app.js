75e2a77ab89fadef30ddf03a43759c1fc8f13fc8
Corey Jackson <cjaxsn@gmail.com>
2026-06-22 18:49:51 -0400

    multiple js files minify

--- app.js
+++ app.js
@@ -30,16 +30,17 @@ npm i html-minifier-terser
 
 console.log("Minify Utility v1 by Jax Tech");
 
-const strFontURL = "http://127.0.0.1:5500/index.html";
+const strFontURL = "http://fightden.ca";
+//const strFontURL = "http://127.0.0.1:5500/index.html";
 //const strFontURL = "https://coreyjackson.me/index.html";
 
 //faLookup();
 
-minifyHTML()
+//minifyHTML()
 //discoverFonts(strFontURL);
 //minifyFonts();
 //minifyFA();
-//minifyJS();
+minifyJS();
 //minifyCSS();
 //minifyImages();
 
@@ -151,8 +152,14 @@ function minifyFonts()
 		}
 	}
 
-	subsetFonts('fonts/fa-brands-400.woff2', "U+20,U+2F,U+E5AE,U+E663,U+F08C,U+F092,U+F16D,U+F368");
-	subsetFonts('fonts/fa-regular-400.woff2', "U+20,U+2F,U+E042,U+E0C6,U+E0D2,U+E0DC,U+E0E9,U+E165,U+E1EC,U+E206,U+E267,U+E3D1,U+E471,U+E4A2,U+E4E5,U+E533,U+E54B,U+E574,U+E5E9,U+E5FD,U+E699,U+E6A1,U+F007,U+F015,U+F023,U+F085,U+F08E,U+F0AE,U+F0B1,U+F0C2,U+F0C5,U+F0D1,U+F0EC,U+F0F2,U+F126,U+F133,U+F19D,U+F1AB,U+F1C0,U+F1C9,U+F228,U+F247,U+F2DB,U+F2EC,U+F37E,U+F3B3,U+F40E,U+F49C,U+F508,U+F52D,U+F534,U+F542,U+F56D,U+F57D,U+F5AB,U+F5BB,U+F5DA,U+F5F3,U+F611,U+F643,U+F647,U+F6FF,U+F78A,U+F7E7,U+F7EA,U+F808,U+F82C");
+	//fight den
+// 	Open Sans": "U+A,U+20-23,U+25-2A,U+2C-3B,U+3E-50,U+52-59,U+5F,U+61-7B,U+7D",
+//   "Lato": "U+20,U+26,U+2D,U+41-50,U+52-59",
+//   "ETmodules": "U+33,U+49,U+4C,U+61",
+//   "Font Awesome 6 Brands": "U+F16D",
+//   "Font Awesome 6 Pro": "U+F060-F062,U+F095" }
+	subsetFonts('fonts/fa-brands-400.woff2', "U+20,U+2F,U+F16D");
+	subsetFonts('fonts/pro-fa-solid-900-0.woff2', "U+20,U+2F,U+F060-F062,U+F095");
 	//subsetFonts('fonts/fa-solid-900.woff2', "");
 
 	//glyphhanger --subset=path/to/fontawesome-solid.ttf --whitelist=U+F007,U+F00c --formats=woff2
@@ -206,13 +213,9 @@ function minifyFA()
 
 function minifyJS()
 {
-	console.log("Minifying JS");
-	const inputFile = 'js/main.js';
-	const outputFile = 'js/main.min.js';
 
-	const sourceCode = fs.readFileSync(inputFile, 'utf8');
-	console.log(sourceCode);
-	
+	console.log("Minifying JS");
+
 	// Configuration options for UglifyJS
     const options = {
         mangle: true, // Shorten variable and function names
@@ -222,61 +225,102 @@ function minifyJS()
             drop_console: false // Set to true to strip console.log statements
         }
     };
-	// Perform minification
-    const result = UglifyJS.minify(sourceCode, options);
 
-    // Check for compilation errors
-    if (result.error) {
-        throw result.error;
+
+	// Find all CSS files in your source directory
+const inputDir = path.join(import.meta.dirname, '/js');
+const outputDir = path.join(import.meta.dirname, '/js/min');
+
+// Ensure output sub-directories exist
+fs.mkdirSync(path.dirname(outputDir), { recursive: true });
+	
+
+ // Find all .css files recursively
+const jsfiles = globSync('*.js', { cwd: inputDir });
+console.log(jsfiles);
+
+jsfiles.forEach(file => {
+	try
+	{
+		const inputFilePath = path.join(inputDir, file);
+		const outputFilePath = outputDir;
+
+		console.log("MINIFYING JS: " + file);
+		// Read input file
+		const fileContent = fs.readFileSync(inputFilePath, 'utf8');
+
+	
+		// Minify JS
+    	const result = UglifyJS.minify(fileContent, options);
+
+		// Check for compilation errors
+		if (result.error) {
+			throw result.error;
+		}
+		
+		const outputFile = path.join(outputFilePath, file.substring(0, file.lastIndexOf("."))) + ".min.js";
+		console.log(outputFile);
+		// Write the minified code to disk
+		fs.writeFileSync(outputFile, result.code);
+		console.log(`Minified: ${file}`);
     }
-
-    // Save the minified code to the output file
-    fs.writeFileSync(outputFile, result.code, 'utf8');
-    console.log(`Success! Minified file saved to: ${outputFile}`);
+	catch (error)
+	{
+		console.log('\x1b[31m%s\x1b[0m', error.message);
+		//console.error('Error during Lightning CSS minify:', error.message);
+	}
+});	
 
 	  console.log("JS Minifying Complete");
 }
 
 function minifyCSS()
 {
-	// Find all CSS files in your source directory
-	const inputDir = path.join(import.meta.dirname, '/css');
-	const outputDir = path.join(import.meta.dirname, '/css/min');
+// Find all CSS files in your source directory
+const inputDir = path.join(import.meta.dirname, '/css');
+const outputDir = path.join(import.meta.dirname, '/css/min');
 
-	 // Find all .css files recursively
-	const cssfiles = globSync('*.css', { cwd: inputDir });
-	console.log(cssfiles);
+// Ensure output sub-directories exist
+fs.mkdirSync(path.dirname(outputDir), { recursive: true });
+	
 
-	cssfiles.forEach(file => {
+ // Find all .css files recursively
+const cssfiles = globSync('*.css', { cwd: inputDir });
+console.log(cssfiles);
+
+cssfiles.forEach(file => {
+	try
+	{
 		const inputFilePath = path.join(inputDir, file);
 		const outputFilePath = outputDir;
-	
+
 		console.log("MINIFYING CSS: " + file);
 		// Read input file
 		const fileContent = fs.readFileSync(inputFilePath);
+
 	
 		// Minify using Lightning CSS
 		const { code } = transform({
-		  filename: file,
-		  code: fileContent,
-		  minify: true,
-		  sourceMap: false, 
+		filename: file,
+		code: fileContent,
+		minify: true,
+		sourceMap: false, 
 		});
-		
-		// Ensure output sub-directories exist
-		fs.mkdirSync(path.dirname(outputFilePath), { recursive: true });
-		
+
 		const outputFile = path.join(outputFilePath, file.substring(0, file.lastIndexOf("."))) + ".min.css";
 		console.log(outputFile);
 		// Write the minified code to disk
 		fs.writeFileSync(outputFile, code);
 		console.log(`Minified: ${file}`);
-	});
+    }
+	catch (error)
+	{
+		console.log('\x1b[31m%s\x1b[0m', error.message);
+		//console.error('Error during Lightning CSS minify:', error.message);
+	}
+});	
+	
 
-	/*
-	// Write the minified payload to disk
-	fs.writeFileSync('css/min/all.min.css', code);
-	*/
 
 	console.log("CSS Minifying Complete");
 }
