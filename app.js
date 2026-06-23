d16bdc4074ded97ad8011fc29ce0d0941828c25d
Corey Jackson <cjaxsn@gmail.com>
2026-06-23 16:33:10 -0400

    fightden font analyze

--- app.js
+++ app.js
@@ -249,7 +249,19 @@ function faLookup()
 function discoverFonts(arrURLs)
 {
 	console.log("Discover Fonts");
-	
+	//fightden.ca
+	//"Font Awesome 6 Brands": "U+F16D"
+	//"Font Awesome 6 Pro": "U+F060,U+F061,U+F073,U+F095,U+F10D,U+F10E,U+F3C5,U+F438,U+F675"
+	//U+F060
+	/*fa-solid*/
+// .fa-boxing-glove { --fa: "\f438"; }
+// .fa-calendar-days { --fa: "\f073"; }
+// .fa-location-dot { --fa: "\f3c5"; }
+// .fa-megaphone { --fa: "\f675"; }
+// .fa-phone { --fa: "\f095"; }
+// .fa-quote-left { --fa: "\f10d"; }
+// .fa-quote-right { --fa: "\f10e"; }
+
 	for (const url of arrURLs)
 	{
 		const command = `npx glyphhanger ${url} --json`;
